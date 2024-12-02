0d383948eb5d22d2b690a6a6c3306a4dfa9408f1
prajwal <prajwalsnavalgi17@gmail.com>
2024-12-02 19:40:09 +0530

    My first Commit

---
+++ src/LED.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+// Define the pin for the LED
+#define LED_PIN 2  // On most ESP32 boards, the built-in LED is connected to GPIO2
+
+void setup() {
+    // Initialize the LED pin as an output
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+    // Turn the LED on
+    digitalWrite(LED_PIN, HIGH);
+    delay(1000);  // Wait for 1 second
+
+    // Turn the LED off
+    digitalWrite(LED_PIN, LOW);
+    delay(1000);  // Wait for 1 second
+}
